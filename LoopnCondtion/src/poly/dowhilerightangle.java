ee846e040137b24fcb213b351955a333ab303c3b
udaisinghrathore <udaisinghrathore@localhost>
2020-06-19 11:57:35 +0530

    pattern

--- LoopnCondtion/src/poly/dowhilerightangle.java
+++ LoopnCondtion/src/poly/dowhilerightangle.java
@@ -1,9 +1,24 @@
+//in a column 
+
 package poly;
 
 public class dowhilerightangle {
 	public static void main(String[] args) {
-	Scanner 
-		
+	int i, j;
+	
+	for (i=1;i<=5;i++) {
+		for (j=1;j<=5;j++) {
+			System.out.println("*");
+			//System.out.println(" ");
+		}
+	}
 	}
 
 }
+
+/*output 
+ * 
+ * 
+ * 
+ * 
+ */
---
+++ LoopnCondtion/src/poly/pattern1.java
@@ -0,0 +1,28 @@
+package poly;
+
+//import java.util.Scanner;
+
+public class pattern1 {
+	
+public static void main(String[] args) 
+{
+	/*System.out.println("Enter the value of n:");
+	Scanner p=new Scanner(System.in);
+	int n=p.nextInt();*/
+	
+	for (int i=1;i<=5;i++) 
+	{
+		
+		for (int j=1; j<=i; j++) 
+			
+		{
+			System.out.print("*");
+			
+		}
+		System.out.println(" ");
+			
+		}
+	
+	
+}
+}
