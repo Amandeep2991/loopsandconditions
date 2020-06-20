dd4b86049e02158b6504df6355b2815cc11e336c
udaisinghrathore <udaisinghrathore@localhost>
2020-06-20 19:41:51 +0530

    pre and post increment

---
+++ LoopnCondtion/src/poly/postincrement.java
@@ -0,0 +1,15 @@
+package poly;
+
+public class postincrement {
+	public static void main(String[] args) {
+		int a=1; 
+		int b=a++;
+		
+		System.out.println("Value of a: "+a);
+		System.out.println("Value of a: "+b);
+		
+		
+	
+	}
+
+}
---
+++ LoopnCondtion/src/poly/preincrement.java
@@ -0,0 +1,16 @@
+package poly;
+
+public class preincrement {
+
+	public static void main(String[] args) {
+		int a=1; 
+		int b=++a;
+		
+		System.out.println("Value of a: "+a);
+		System.out.println("Value of b: "+b);
+		
+		
+	
+	}
+
+}
